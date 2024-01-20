5b9200388eaa895a04277cddc9f55606b4b8d049
Akbar Nafisa <akbarnj788@gmail.com>
2024-01-20 12:26:44 +0700

    refactor: update API path to use root

--- apps/client/src/data/todo.ts
+++ apps/client/src/data/todo.ts
@@ -1,10 +1,8 @@
 import request from "../utils/request";
 import { GenericResponse, Todo, TodoDto } from "../use-case/todo";
 
-const API_URL = import.meta.env.VITE_API_URL
-
 export async function getTodos(): Promise<TodoDto> {
-  const res = await request(`${API_URL}/api/todo`, {
+  const res = await request('/api/todo', {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
@@ -15,7 +13,7 @@ export async function getTodos(): Promise<TodoDto> {
 }
 
 export async function createTodo(title: Todo["title"]): Promise<GenericResponse> {
-  const res = await request(`${API_URL}/api/todo`, {
+  const res = await request('/api/todo', {
     method: "POST",
     body: JSON.stringify({ title }),
     headers: {
@@ -27,7 +25,7 @@ export async function createTodo(title: Todo["title"]): Promise<GenericResponse>
 }
 
 export async function deleteTodo(id: Todo["id"]): Promise<GenericResponse> {
-  const res = await request(`${API_URL}/api/todo/${id}`, {
+  const res = await request(`/api/todo/${id}`, {
     method: "DELETE",
     headers: {
       "Content-Type": "application/json",
